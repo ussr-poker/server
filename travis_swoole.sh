4682111f03b0f6d97e963f4d12dd05489e32bba8
coder <coder1994@gmail.com>
2019-08-21 21:59:51 +0300

    try to cache swoole ext

--- travis_swoole.sh
+++ travis_swoole.sh
@@ -2,17 +2,20 @@
 
 cd ~
 
-if [[ -d "swoole-src" ]]
+if [[ -f "swoole-src/libs/swoole.so" ]]
 then
     cd swoole-src
-    make install
+    echo "swoole.so exists"
 else
+    echo "swoole.so not existing, build started..."
+
     git clone https://github.com/swoole/swoole-src.git
     cd swoole-src
     phpize
     ./configure --enable-sockets
-    make -j 4 && make install
-    cd ../
+    make -j 4
 fi
+
+make install
 
 echo "extension = swoole.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
