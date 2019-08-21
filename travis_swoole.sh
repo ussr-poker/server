de4005ce6a437c838be158a2cc18a1211321dd60
coder <coder1994@gmail.com>
2019-08-21 22:23:34 +0300

    try to cache swoole ext

--- travis_swoole.sh
+++ travis_swoole.sh
@@ -5,7 +5,12 @@ cd ~
 if [[ -f "swoole-src/.libs/swoole.so" ]]
 then
     cd swoole-src
+
     cp .libs/CMakeLists.txt ./CMakeLists.txt
+    cp .libs/Makefile ./Makefile
+    cp .libs/Makefile.fragments ./Makefile.fragments
+    cp .libs/Makefile.objects ./Makefile.objects
+
     echo "swoole.so exists"
 else
     echo "swoole.so not existing, build started..."
@@ -14,10 +19,15 @@ else
 
     git clone https://github.com/swoole/swoole-src.git
     cd swoole-src
-    mkdir .libs && cp CMakeLists.txt .libs/CMakeLists.txt
     phpize
     ./configure --enable-sockets
     make -j 4
+
+    mkdir .libs
+    cp CMakeLists.txt .libs/CMakeLists.txt
+    cp Makefile .libs/Makefile
+    cp Makefile.fragments .libs/Makefile.fragments
+    cp Makefile.objects .libs/Makefile.objects
 fi
 
 make install
